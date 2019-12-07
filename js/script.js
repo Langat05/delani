791a1501b6ae132be14f2a8b2c57bdd41f9f2097
Langat <justuslangat78@gmail.com>
2019-12-07 12:30:56 +0300

    modified the CSS file and js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         $("#design").show();
         $("#design-image").show();
     });
-
+    
     $("#development-image").click(function () {
         $("#development-image").hide();
         $("#development").show();
@@ -27,4 +27,32 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $("#project1").mouseover(function () {
+        $("#overlay").show();
+    }).mouseout(function () {
+        $("#overlay").hide();
+    });
+});
+$(document).ready(function () {
+    $("#project2").mouseover(function () {
+        $("#overlay1").show();
+    }).mouseout(function () {
+        $("#overlay1").hide();
+    });
+});
+$(document).ready(function () {
+    $("#project3").mouseover(function () {
+        $("#overlay2").show();
+    }).mouseout(function () {
+        $("#overlay2").hide();
+    });
+});
+$(document).ready(function () {
+    $("#project4").mouseover(function () {
+        $("#overlay3").show();
+    }).mouseout(function () {
+        $("#overlay3").hide();
+    });
+});
 
